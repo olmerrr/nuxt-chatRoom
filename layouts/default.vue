0181898e89851c334a768bde1d60ab288072e8d5
Yaroslav <dombrovskiy91@gmail.com>
2020-12-24 10:49:35 +0200

    more style + litle logic with data def layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,52 @@
 <template>
-  <v-app app>
-    <nuxt></nuxt>
+  <v-app app dark>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list subheader>
+        <v-subheader>People's List:</v-subheader>
+        <v-list-tile v-for="u in users" :key="u.id" @click.prevent>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ u.name }}</v-list-tile-title>
+          </v-list-tile-content>
+
+          <v-list-tile-action>
+            <v-icon :color="u.id === 2 ? 'primary' : 'grey'"
+              >chat_bubble</v-icon
+            >
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click="exit">
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+      <v-toolbar-title>Chat Room {{ user.room }}</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <div>
+        <nuxt />
+      </div>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      drawer: true,
+      users: [
+        { id: 1, name: "User 1" },
+        { id: 2, name: "User 2" },
+      ],
+    };
+  },
+  computed: mapState(["user"]),
+  methods: {
+    exit() {},
+  },
+};
 </script>
