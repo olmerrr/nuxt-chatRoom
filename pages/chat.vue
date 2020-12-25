f4b54a0ce88908060afb796ecdaf24e5510856bd
Yaroslav <dombrovskiy91@gmail.com>
2020-12-25 17:40:40 +0200

    add users on server + connect socket.io + start writing msg with users

--- pages/chat.vue
+++ pages/chat.vue
@@ -5,7 +5,7 @@
               :key="message.text"
               :name="message.name"
               :text="message.text"
-              owner
+              :owner="message.id === user.id"
                />
     </div>
     <div class="c-form">
--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const app = require ('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const m = (name, text, id) => ({ name, text, id}); 
+const users = require('./users')();
 
 io.on('connection', socket => {
   socket.on("userJoined", (data, cb) => {
@@ -9,9 +10,14 @@ io.on('connection', socket => {
       return cb('Error with Data')
     } else {
       socket.join(data.room);
+      users.remove(socket.id);
+      users.add({
+        id: socket.id,
+        name: data.name,
+        room: data.room
+      });
       cb({ userId: socket.id })
       socket.emit('newMessage', m('admin', `Hello, ${data.name}.`))
-      socket.emit('newMessage', m('TEST', `Bonjorno ;)`))
 
       socket.broadcast
         .to(data.room)
@@ -20,16 +26,17 @@ io.on('connection', socket => {
 
   })
 
-  socket.on('createMessage', data => {
-    setTimeout(() => {
-      socket.emit('newMessage', {
-        text: data.text + ' SERVER'
-      })
-    }, 500)
-  
-  });
-  
-})
+  socket.on('createMessage', (data, cb) => {
+  if (!data.text) {
+    return cb("Text can't be empty..");
+  }
+  const user = users.get(data.id);
+  if (user){
+    io.to(user.room).emit('newMessage', m(user.name, data.text, data.id))
+    }
+    cb()
+  })
+});
 
 module.exports = {
   app,
